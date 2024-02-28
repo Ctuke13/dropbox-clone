3d20f514e91ff8895340f7c07f58a7ece20fbb03
Chidi Tuke <c2kproductions@gmail.com>
2024-02-28 12:20:18 -0500

    complete homepage

---
+++ app/page.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className="flex flex-col lg:flex-row items-center bg-[#1E1919] dark:bg-slate-800 ">
+        <div className="p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5">
+          <h1 className="text-5xl font-bold">
+            Welcome to Dropbox.
+            <br />
+            <br />
+            Storing everything for you and your business needs. All in one
+            place.
+          </h1>
+
+          <p>
+            Enhance your personal storage with Dropbox, offering a simple and
+            efficient way to upload, organize, and access files from anywhere.
+            Securely store important documents and media, and experience the
+            convenience of easy file management and sharing in one centralized
+            solution.
+          </p>
+
+          <Link
+            href="/dashboard"
+            className="flex cursor-pointer bg-blue-500 p-5 w-fit rounded-lg"
+          >
+            Try it for free!
+            <ArrowRight className="" />
+          </Link>
+        </div>
+        <div className="bg-[#1E1919] dark:bg-slate-800 h-full p-10">
+          <video autoPlay loop muted className="rounded-lg">
+            <source
+              src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/dropbox/dbx1-hero-1920x1080.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+      </div>
+      <p className="text-center font-bold text-xl pt-5">Disclaimer</p>
+      <p className="text-center font-light p-2">
+        This project is made for educational purposes only. I do not own or
+        affiliate with Dropbox or/and any of its subsidiaries in any form.
+      </p>
+    </main>
+  );
+}
